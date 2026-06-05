11f63a9aa95516ebc86d093f6626f3e2ed4b0dee
Nikolai Dorofeev <contact@d0rich.me>
2026-06-05 08:37:48 +0200

    Chat handler interface

---
+++ src/participant/graphensResponder.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode'
+
+
+const graphensResponder: vscode.ChatRequestHandler = async (
+  request: vscode.ChatRequest,
+  context: vscode.ChatContext,
+  stream: vscode.ChatResponseStream,
+  token: vscode.CancellationToken
+): Promise<void> => {
+  
+};
